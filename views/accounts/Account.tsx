55f258598998a80ffbae33e7540a23c63456a4b3
Sergey Elpashev <elpashevs@bk.ru>
2023-02-26 13:34:29 +0300

    Added blank screens

---
+++ views/accounts/Account.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Account(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>This is Account screen!</Text>
+        </View>
+    )
+}
---
+++ views/debts/Debt.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Debt() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>This is Debt screen!</Text>
+        </View>
+    )
+}
