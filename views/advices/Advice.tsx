78a6dbb002cfd49c1dde06f875ba016a05f3f296
Sergey Elpashev <elpashevs@bk.ru>
2023-02-28 14:53:36 +0300

    Added advices screen

---
+++ views/advices/Advice.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Advice() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>This is Advices screen!</Text>
+        </View>
+    )
+}
