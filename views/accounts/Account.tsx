87feddf2af9f394e668ff1cf1f355c625ede25bd
Sergey Elpashev <elpashevs@bk.ru>
2023-03-02 23:05:46 +0300

    Debt screen with some cards (no pain)

--- views/accounts/Account.tsx
+++ views/accounts/Account.tsx
@@ -1,9 +1,149 @@
 import { Text, View } from "react-native";
+import styled from "styled-components/native";
+
+import Header from "../modular_components/Header";
+import Card from "../modular_components/Card";
+
+
+const ActiveIndicator = styled.View`
+    background-color: #6FE6C2;
+    height: 20px;
+    width: 20px;
+`
+const InactiveIndicator = styled.View`
+    background-color: #D9D9D9;
+    height: 20px;
+    width: 20px;
+`
+
+const Scroll = styled.ScrollView`
+    margin: 0;
+    max-height: 100%;
+`
+const Container = styled.View`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+`
 
 export default function Account(){
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>This is Account screen!</Text>
+        <View style={{ backgroundColor: '#FFF', height: '100%' }}>
+            <Header name="Accounts" style="1"/>
+            <Scroll>
+                <Container>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <ActiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                    <Card>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                                <InactiveIndicator/>
+                                <Text style={{marginLeft: 10}}>Active</Text>
+                            </View>
+                            <Text>5000 руб.</Text>
+                        </View>
+                    </Card>
+                </Container>
+            </Scroll>
         </View>
     )
 }
