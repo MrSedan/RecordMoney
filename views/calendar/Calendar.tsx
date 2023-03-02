0d311d204a26e22ff1cd28b445b07d81e82c45cd
shin <igoshin.rusl@yandex.ru>
2023-03-02 23:31:22 +0300

    Calendar Page

--- views/calendar/Calendar.tsx
+++ views/calendar/Calendar.tsx
@@ -1,9 +1,24 @@
 import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import Header from '../modular_components/Header';
+import Content from './calendar_content/Content';
+import Card from '../modular_components/Card';
+
+const CardText = styled.Text`
+    font-size: 13px;
+    font-weight: 400;
+`;
 
 export default function Calendar() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>This is Calendar screen!</Text>
+        <View style={{ backgroundColor:'#FFF', height: '100%'}}>
+            <Header name='Calendar' style='1'/>
+            <Content />
+            <Card>
+                <CardText>Зарплата</CardText>
+                <CardText>09.03.2023</CardText>
+                <CardText>40000 руб.</CardText>
+            </Card>
         </View>
     )
 }
