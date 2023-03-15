6816ef25a8a7cec10d9796f1a18ed3a97e0e7910
Sergey Elpashev <elpashevs@bk.ru>
2023-03-15 21:59:55 +0300

    Functions for work with accounts

--- views/tools/account.ts
+++ views/tools/account.ts
@@ -1,10 +1,40 @@
-import { account, emptyAccount } from "../../models/interfaces";
-import { getData } from "./iosys";
+import { account, emptyAccount } from '../../models/interfaces';
+import { getData } from './iosys';
 
-export async function getAccounts(){
-    let data: account = await getData({fileName: 'Account'});
+/**
+ * Функция получения списка доступных счетов
+ * @returns Возвращает массив счетов
+ */
+export async function getAccounts() {
+    let data: account = await getData({ fileName: 'Account' });
     if (data === null) {
-        data = emptyAccount()
+        data = emptyAccount();
     }
-    return data.accounts
+    return data.accounts;
+}
+
+/**
+ * Функция работы с деньгами на счете. При невозможности операции (недостаточно средств) вызовет ошибку,
+ * которую нужно обрабатывать при помощи `try` и `catch` конструкций
+ * @param count - количество денег, что поступят на счет
+ * @param id_account - идентификатор счета, с которым происходит операция
+ * @example ```ts
+ * try {
+ *  addMoney(-900, 0)
+ * } catch {
+ * console.log("Недостаточно средств")
+ * }
+ * ```
+ */
+export async function addMoney(count: number, id_account: string | number) {
+    let data: account = await getData({ fileName: 'Account' });
+    data.accounts.map((item) => {
+        if (item.id == Number(id_account)) {
+            if (item.id == Number(id_account)) {
+                if (item.sum + count < 0) throw Error('Не хватает средств!');
+                item.sum += count;
+                return;
+            }
+        }
+    });
 }
