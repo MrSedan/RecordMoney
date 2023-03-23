c16246409d3fc01d2556f15e34c9d027815767ec
Sergey Elpashev <elpashevs@bk.ru>
2023-03-24 00:03:39 +0300

    Remove data on click for testing

--- views/advices/Advice.tsx
+++ views/advices/Advice.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
 
-export default function Advice() {
+// export default function Advice() {
+//     return (
+//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//             <Text>This is Advices screen!</Text>
+//         </View>
+//     )
+// }
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { removeAllData } from '../tools/iosys';
+
+export default function CardSwipe() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>This is Advices screen!</Text>
+            <TouchableOpacity
+                style={{ width: 100, height: 100, backgroundColor: 'red' }}
+                onPress={() => {
+                    removeAllData();
+                }}
+            />
         </View>
-    )
+    );
 }
