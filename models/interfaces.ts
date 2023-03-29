063e12ef8fb371b98303bfb3c51e75a3d51db77b
shin1an <igoshin.rusl@yandex.ru>
2023-03-29 19:58:37 +0300

    Fixed and Added functions

--- models/interfaces.ts
+++ models/interfaces.ts
@@ -1,129 +1,111 @@
 export interface category {
-    categories: 
-        {
-            id: number,
-            name: string,
-            category_icon: number,
-            category_type: string,
-            color: string,
-            value: number,
-        }[]
+    categories: {
+        id: number;
+        name: string;
+        category_icon: number;
+        category_type: string;
+        color: string;
+        value: number;
+    }[];
 }
 export interface history {
-    history: 
-        {
-            id: number,
-            id_account: number,
-            category: number,
-            date: string,
-            sum: number,
-            comment: string
-        }[]
-    
+    history: {
+        id: number;
+        id_account: number;
+        category: number;
+        date: string;
+        sum: number;
+        comment: string;
+    }[];
 }
 
-export function emptyCategories():category {
-    let newData: category = {categories:[]};
-    return newData
+export function emptyCategories(): category {
+    let newData: category = { categories: [] };
+    return newData;
 }
 
-export function emptyHistory():history  {
-    let newData: history= { history:[]};
-    return newData
+export function emptyHistory(): history {
+    let newData: history = { history: [] };
+    return newData;
 }
-
 
 export interface account {
-    accounts: 
-        {
-            id: number,
-            name: string,
-            sum: number
-        }[]
+    accounts: {
+        id: number;
+        name: string;
+        sum: number;
+    }[];
 }
 
 export function emptyAccount(): account {
-    let newData: account = {accounts: []};
-    return newData
+    let newData: account = { accounts: [] };
+    return newData;
 }
 
 export interface accountHistory {
-    accHistory: 
-        {
-            id: number,
-            id_operation: number,
-            id_account: number,
-            date: string,
-            sum: number,
-            type: string,
-            page: "home"| "calendar" | "debt" | "piggyBank",
-        }[]
+    accHistory: {
+        id: number;
+        id_operation: number;
+        id_account: number;
+        date: string;
+        sum: number;
+        type: string;
+        page: 'home' | 'calendar' | 'debt' | 'piggyBank';
+    }[];
 }
 
 export function emptyAccHistory(): accountHistory {
-    let newData: accountHistory = {accHistory: []};
-    return newData
+    let newData: accountHistory = { accHistory: [] };
+    return newData;
 }
 
-
-
 export interface piggyBank {
-    piggyBanks: 
-        {
-            id: number,
-            id_account: number,
-            name: string,
-            sum_max: number,
-            sum_cur: number,
-            status: boolean
-        }[]
+    piggyBanks: {
+        id: number;
+        name: string;
+        sum_max: number;
+        sum_cur: number;
+        status: boolean;
+    }[];
 }
 
 export function emptyPiggyBank(): piggyBank {
-    let newData: piggyBank = {piggyBanks: []};
-    return newData
+    let newData: piggyBank = { piggyBanks: [] };
+    return newData;
 }
 
-
-
-
 export interface debt {
-    debts: 
-        {
-            id: number,
-            id_account: number,
-            name: string,
-            contact: string,
-            type: string,
-            sum: number,
-            date: string,
-            comment: string
-        }[]
+    debts: {
+        id: number;
+        id_account: number;
+        name: string;
+        contact: string;
+        type: string;
+        sum: number;
+        date: string;
+        comment: string;
+    }[];
 }
 
 export function emptyDebt(): debt {
-    let newData: debt = {debts: []};
-    return newData
+    let newData: debt = { debts: [] };
+    return newData;
 }
-
-
-
 
 export interface calendar {
-    cards: 
-        {
-        id: number,
-        id_account: number,
-        date: string,
-        name: string,
-        type: string,
-        sum: number,
-        comment: string,
-        close: boolean
-        }[]
+    cards: {
+        id: number;
+        id_account: number;
+        date: string;
+        name: string;
+        type: string;
+        sum: number;
+        comment: string;
+        close: boolean;
+    }[];
 }
 
-export function emptyCalendar():calendar {
-    let newData: calendar = {cards: []};
-    return newData
+export function emptyCalendar(): calendar {
+    let newData: calendar = { cards: [] };
+    return newData;
 }
